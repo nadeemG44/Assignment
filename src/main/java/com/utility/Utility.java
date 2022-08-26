f7ffe90e64477f3ecb038b8ba477badb04306808
Nadeem <nadeemgavandi44@gmail.com>
2022-08-26 18:57:07 +0530

    utility and test class updated

--- src/main/java/com/utility/Utility.java
+++ src/main/java/com/utility/Utility.java
@@ -19,6 +19,100 @@ public class Utility
 		FileHandler.copy(src, desti);
 	}
 
+
+
+	public List<String> sortByProductName(WebDriver driver)
+	{	
+		driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
+		List<WebElement> beforeFilter =	driver.findElements(By.className("product-item-link")); 
+		  
+		List<String> li=new  ArrayList<String>();
+				for(WebElement bf:beforeFilter)
+						{ 
+							String elementsBeforeSort = bf.getText();
+							System.out.println(elementsBeforeSort);
+							li.add(elementsBeforeSort); 
+						}
+							Collections.sort(li);
+		  //--------------------------------------------------------------------------  
+	         driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
+	 List<String> li1=new ArrayList<String>();
+		    
+		 try {
+			 	WebElement sortBy1=driver.findElement(By.xpath("(//select[@id='sorter'])[1]")); 
+		      	Select s=new Select(sortBy1); 
+		      	s.selectByValue("name");
+		      		List<WebElement> afterSortByName=driver.findElements(By.className("product-item-link"));
+				 //	List<String> finalSort2 = li1.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+
+					for(WebElement af:afterSortByName)
+					{
+						String elementsAfterSort = af.getText();
+						li1.add(elementsAfterSort); 
+					}
+					Collections.sort(li1);
+			} 
+		 	catch (StaleElementReferenceException e) 
+		 			{
+		 			e.getMessage();
+		 			}   
+		 
+        List<String> list=new ArrayList<>();
+		  	list.addAll(li);
+		  	list.addAll(li1);
+		  	
+		  	return list;
+	      		      	
+	}
+	
+	
+	
+	public List<String> sortByProductPrice(WebDriver driver)
+	{
+		List<WebElement> beforeSort = driver.findElements(By.xpath("//span[@class='special-price']//span[@class='price']"));
+		List<String> li=new ArrayList<>();
+
+		for (WebElement bf : beforeSort) 
+ 		{
+ 			String text = bf.getText().replaceAll("[^0-9]","");
+ 			li.add(text);
+ 			System.out.println(text);
+ 		}
+		
+		Collections.sort(li);
+		
+		
+		
+		List<String> li1=new ArrayList<>();
+		try {
+			WebElement sortBy1=driver.findElement(By.xpath("(//select[@id='sorter'])[1]"));
+		     Select s=new Select(sortBy1); 
+		     s.selectByValue("price");
+		     
+		     List<WebElement> aftereSort = driver.findElements(By.xpath("//span[@class='special-price']//span[@class='price']"));
+			
+		     for (WebElement af : aftereSort) 
+				{
+					String text = af.getText().replaceAll("[^0-9]","");
+					System.out.println(text);
+					li1.add(text);
+				}
+		     Collections.sort(li1);
+			} 
+			catch (StaleElementReferenceException e) 
+				{
+					e.getMessage();
+				}
+		
+		List<String> list=new ArrayList<>();
+	  	list.addAll(li);
+	  	list.addAll(li1);
+		
+		return list;
+		
+	}
+	
+	
 	
 	
 	
--- src/test/java/com/testClasses/SortBy_productName.java
+++ src/test/java/com/testClasses/SortBy_productName.java
@@ -30,143 +30,44 @@ public class SortBy_productName extends BaseClass {
 	BaseClass baseClass;
 	String TCID;
 	WebDriver driver;
-
+	
+	
 	@BeforeMethod
 	public void objInitialization() {
 		driver = launchBrowser();
+		
 		shopByCategory = new ShopBy_category(driver);
 		utility = new Utility();
-	}
+		}
 
 	
 	  @Test
 	  public void SortByProductName() throws InterruptedException 
 	  {
+		  TCID="102";
+		  Reporter.log("TEST-CASE-01 failed: Before-sorted elements compaired");
 	  
-	  // id is set for screenshot
-		  	TCID="102";
-	  
-	  // logs are provided for failed test case in emailable report
-		  	Reporter.log("TEST-CASE-01 failed: Before-sorted elements compaired");
-	  
-	  
-	   // All the product-names on webPage are taking into variable by xpath
-		  	List<WebElement> beforeFilter =	driver.findElements(By.className("product-item-link")); 
-		  
-		 // Generic ArrayList created to add webElements in form of string
-		  		List<String> li=new  ArrayList<String>();
-	  
-		  		
-		
-		  		//  All webElements are iterated using for-loop
-		  					for(WebElement bf:beforeFilter)
-		  						{ 
-		  							String elementsBeforeSort = bf.getText();
-		  							li.add(elementsBeforeSort); 
-		  						}
-		  					
-		  			
-		  		// string elements (list) sorted in asending order
-		  				Collections.sort(li);
-		  						//	Collections.sort(li,Collections.reverseOrder());
-	  
-	  //---------------------------------------------------------------------------
+		  	List<String> raisa = utility.sortByProductName(driver);
+		  	Collections.sort(raisa);
+		  	/*String string = raisa.get(0);
+		  	String string2 = raisa.get(1);*/
+		  		Assert.assertTrue(raisa.get(0).equals(raisa.get(1)));
+	  }
 	  
 	  
-	      driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
-	  
-	     //  drop-down handled by using Select class
-	      	WebElement sortBy1=driver.findElement(By.xpath("(//select[@id='sorter'])[1]")); 
-	      	Select s=new Select(sortBy1); 
-	      	s.selectByValue("name");
-	  
-	  
-	  
-	     // All the product-names on webPage are taking into variable by xpath
-	      	List<WebElement> afterSortByName=driver.findElements(By.className("product-item-link"));
-	  
-	       // Generic ArrayList created to add webElements in form of string
-	      		List<String> li1=new ArrayList<String>(); 
-	 
-	      		 //  not-sorted web elements are sorted by "stream()"		
-	      			List<String> finalSort2 = li1.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-	  
-	      		//  All webElements are iterated using for-loop
-	      				for(WebElement af:afterSortByName)
-	      					{
-	      						String elementsAfterSort = af.getText();
-	      						li1.add(elementsAfterSort); 
-	      					}
-	  
-	      				//// string elements (list) sorted in asending order
-	      						//Collections.sort(li1);
-	  
-	   //  Assertion is used to verify the outcome
-	   Assert.assertTrue(li.equals(li1));
-	   
-  }
-	 
     @Test(priority=1)
 	public void SortByProductPrice() 
 	{
-		
-	//   id is set for screenshot
 		 TCID="103";
-		 
-	   // logs are provided for faild test case in emailable report 
-		  Reporter.log("TEST-CASE-02 failed: Non-sorted elements compared with  Sorted-Elements ");
+	     Reporter.log("TEST-CASE-02 failed: Non-sorted elements compared with  Sorted-Elements ");
 		  
-         //  All the product-names on webPage are taking into variable by xpath
-		  	List<WebElement> beforeSort = driver.findElements(By.xpath("//span[@class='special-price']//span[@class='price']"));
 		
-		  	
-		   // Generic ArrayList created to add webElements in form of string
-		         List<String> li1=new ArrayList<>();
-
-		     //  All webElements are iterated using for-loop
-		         	for (WebElement bf : beforeSort) 
-		         		{
-		         			String text = bf.getText().replaceAll("[^0-9]","");
-		         			li1.add(text);
-		         			System.out.println(text);
-		         		}
-		         	
-	//  non-sorted web elements are sorted by	
-		Collections.sort(li1);
-		
-/*------------------------------------------------------------------------------------------------------------*/
-		
-		 driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
-		 
-	//  All the product-names on webPage are taking into variable by xpath 
-		
-       //  drop-down handled by using Select class
-		     WebElement sortBy1=driver.findElement(By.xpath("(//select[@id='sorter'])[1]"));
-		     Select s=new Select(sortBy1); 
-		     s.selectByValue("price");
-
-		    // All the product-names on webPage are taking into variable by xpath
-		       List<WebElement> aftereSort = driver.findElements(By.xpath("//span[@class='special-price']//span[@class='price']"));
-		
-		 
-      // Generic ArrayList created to add webElements in form of string		       
-		List<String> li2=new ArrayList<>();
-		
-		  //  All webElements are iterated using for-loop	
-			for (WebElement af : aftereSort) 
-				{
-					String text = af.getText().replaceAll("[^0-9]","");
-					System.out.println(text);
-					li2.add(text);
-				}
-			
-		//  non-sorted web elements are sorted by	
-		//	Collections.sort(li2);
-			
-	      //  Assertion is used to verify the outcome
-		      Assert.assertTrue(li1.equals(li2));
+    	List<String> raisa = utility.sortByProductPrice(driver);
+	  Collections.sort(raisa);
+	  	String string = raisa.get(0);
+	  	String string2 = raisa.get(1);
+	  		Assert.assertTrue(string.equals(string2));
 	}
-    
     
     
      @AfterMethod
@@ -181,10 +82,10 @@ public class SortBy_productName extends BaseClass {
 	  }
 
 	
-	/*  @AfterClass 
+	  @AfterClass 
 	  public void closeTab() 
 	  { driver.close(); 
-	  }*/
+	  }
 	 
 
 }
